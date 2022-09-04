35f5b1212c898127dfd38ea929033f3da73c51a2
angeldarco <angel92-eme@hotmail.com>
2022-09-04 15:07:37 +0000

    main page add

--- src/pages/body/main/Main.jsx
+++ src/pages/body/main/Main.jsx
@@ -1,9 +1,30 @@
 import './Main.scss';
+import logo from '../../../assets/img/owner.png';
 
 const Main = ()=>{
     return (
         <div className='mainContainer'>
-            <h1>Main</h1>
+            <div className="body">
+                <div className="header">
+                <ul>
+                <li>Home</li>
+                <li>Proyects</li>
+                <li>About me</li>
+                <li>Contact</li>
+            </ul>
+            <button>Hire Me</button>
+                </div>
+            <div className="logo">
+                <img src={logo} alt="logo.gif" /> 
+            </div>
+            <div className="text">
+                <h5></h5>
+                <h1>Frontend Developer</h1>
+                <h3>Mid/Level</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum temporibus eveniet aliquam culpa explicabo atque, incidunt tempora nihil exercitationem odit ratione in itaque, accusamus dolore voluptas, at facilis dolores voluptate.</p>
+                <h6></h6>
+            </div>
+            </div>
         </div>
     )
 }
