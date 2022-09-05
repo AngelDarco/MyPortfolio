c25bed7e27cb1001384c5c2c193875dc84384614
angeldarco <angel92-eme@hotmail.com>
2022-09-05 17:12:01 +0000

    contact add

---
+++ src/pages/body/contact/Contact.jsx
@@ -0,0 +1,25 @@
+import './Contact.scss';
+import { MdAlternateEmail } from 'react-icons/md';
+import { AiFillGithub } from 'react-icons/ai';
+import { SiGmail } from 'react-icons/si';
+import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
+
+
+const Contact = ()=>{
+    return(
+        <div className="containerContact">
+            <div className="body">
+                <h1>Contact</h1>
+                <div className="icons">
+                    <MdAlternateEmail/>
+                    <AiFillGithub/>
+                    <SiGmail/>
+                    <FaFacebookF/>
+                    <FaTwitter/>
+                    <FaInstagram/>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Contact;
