4346113dd14345679aab6df0a92f8eb0829a7a93
angeldarco <angel92-eme@hotmail.com>
2022-09-05 06:25:38 +0000

    footer add

--- src/pages/body/footer/Footer.jsx
+++ src/pages/body/footer/Footer.jsx
@@ -3,7 +3,10 @@ import './Footer.scss';
 const Footer = ()=>{
     return(
         <div className="footerContainer">
-            <h1>Footer</h1>
+            <div className="body">
+                 <h2>Portfolio <span>Created and developed by Angel GP</span> </h2>
+                 <p>Copyright © 1992-2022 Darco Group, All Rights Reserved.</p>
+            </div>
         </div>
     )
 }
