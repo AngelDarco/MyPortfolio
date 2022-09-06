1f02d5408bbbbac7eafc92a95d1f743376d7d02a
angeldarco <angel92-eme@hotmail.com>
2022-09-06 17:38:03 +0000

    darkmode localstorage add

--- src/pages/body/darkmode/DarkMode.jsx
+++ src/pages/body/darkmode/DarkMode.jsx
@@ -1,27 +1,32 @@
 import './DarkMode.scss';
 import { MdDarkMode, MdOutlineWbSunny } from 'react-icons/md';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const DarkMode = ()=>{
-    const [ darkMode, setDarkMode ] = useState(false)
-    const handlerDarkMode = (e)=>{
-        // if(e.target.classList.contains('light')){
-            setDarkMode(!darkMode)
-            if(!darkMode){
-                document.documentElement.style.setProperty('--primary-color', '#303131');
-                document.documentElement.style.setProperty('--secondary-color', '#000');
-                document.documentElement.style.setProperty('--text-color', '#fff');
-            }else{
-                document.documentElement.style.setProperty('--primary-color', '#e0dede');
-                document.documentElement.style.setProperty('--secondary-color', '#fff');
-                document.documentElement.style.setProperty('--text-color', '#000');
-            }
-        
+    const local = localStorage.getItem('darkmode') || false;
+    const [ darkMode, setDarkMode ] = useState(local);
+
+    const handlerDarkMode = ()=> {
+        setDarkMode(!darkMode);
+        darkMode
+        ? localStorage.setItem('darkmode','light')
+        : localStorage.setItem('darkmode','dark')
     }
+    
+    if(local === 'dark'){
+        document.documentElement.style.setProperty('--primary-color', '#303131');
+        document.documentElement.style.setProperty('--secondary-color', '#000');
+        document.documentElement.style.setProperty('--text-color', '#fff');
+    }else{
+        document.documentElement.style.setProperty('--primary-color', '#e0dede');
+        document.documentElement.style.setProperty('--secondary-color', '#fff');
+        document.documentElement.style.setProperty('--text-color', '#000');
+    }
+
     return(
-        <div className="containerDarkMode" >
-            <MdDarkMode className={darkMode && 'dark'} onClick={handlerDarkMode}/>
-            <MdOutlineWbSunny className={!darkMode &&'light'} onClick={handlerDarkMode}/>
+        <div className="containerDarkMode">
+            <MdDarkMode className={local === 'dark' && 'dark'} onClick={handlerDarkMode}/>
+            <MdOutlineWbSunny className={local === 'light' &&'light'} onClick={handlerDarkMode}/>
         </div>
     )
 }
