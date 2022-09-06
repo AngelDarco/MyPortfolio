e7b0e9644a62c613cc2fbdd580f6ed6aacebea85
angeldarco <angel92-eme@hotmail.com>
2022-09-06 17:40:06 +0000

    components add

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import './App.css';
+import Proyects from './pages/body/proyects/Proyects';
+import About from './pages/body/about/About';
+import Footer from "./pages/body/footer/Footer"
+import Main from "./pages/body/main/Main"
+import Contact from './pages/body/contact/Contact';
+import DarkMode from './pages/body/darkmode/DarkMode';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <DarkMode/>
+       <Main/>
+       <About/>
+       <Proyects/>
+       <Contact/>
+      <Footer/>
     </div>
   )
 }
