a1919e9b30ccb47f83a1b9467fd2b50dc75dc819
angeldarco <angel92-eme@hotmail.com>
2022-09-13 19:52:24 +0000

    V_1.0 finished

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import Footer from "./pages/body/footer/Footer"
 import Main from "./pages/body/main/Main"
 import Contact from './pages/body/contact/Contact';
 import DarkMode from './pages/body/darkmode/DarkMode';
+import Header from './pages/body/header/Header';
 
 function App() {
   return (
     <div className="App">
       <DarkMode/>
+      <Header/>
        <Main/>
        <About/>
        <Proyects/>
