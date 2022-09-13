84a4be6d7e3d482fcb4e03d361ef49a263a1bca8
angeldarco <angel92-eme@hotmail.com>
2022-09-13 19:51:56 +0000

    main updated

--- src/pages/body/main/Main.jsx
+++ src/pages/body/main/Main.jsx
@@ -1,59 +1,10 @@
 import './Main.scss';
 import logo from '../../../assets/img/owner.png';
-import { GiHamburgerMenu } from 'react-icons/gi';
-import { GrClose } from 'react-icons/gr';
-import { useState } from 'react';
 
 const Main = ()=>{
-    const [ showMenu, setShowMenu ] = useState(false);
-    const handlerMenu = ()=> setShowMenu(!showMenu);
-
-    const handlerLinks = (e)=>{
-        switch(e.target.innerText){
-            case 'HOME':
-                window.location.href = '#home';
-
-            break;
-
-            case 'PROYECTS':
-                window.location.href = '#proyects';
-
-            break;
-
-            case 'ABOUT ME':
-                window.location.href = '#about';
-
-            break;
-
-            case 'CONTACT':
-                window.location.href = '#contact';
-
-            break;
-
-            default:
-                break;            
-        }
-    }
-
     return (
-        <div className='mainContainer'>
+        <div className='mainContainer' id='home'>
             <div className="body">
-                    <div className='menu' onClick={handlerMenu}>
-                        {
-                           showMenu ? <GrClose className='close'/>
-                                    : <GiHamburgerMenu />
-                        }
-                    </div>
-                <div className={`header ${!showMenu ? 'hide' : ''}`}>
-                
-                <ul>
-                <li onClick={handlerLinks}>Home</li>
-                <li onClick={handlerLinks}>Proyects</li>
-                <li onClick={handlerLinks}>About me</li>
-                <li onClick={handlerLinks}>Contact</li>
-            </ul>
-            <button>Hire Me</button>
-                </div>
             <div className="logo">
                 <img src={logo} alt="logo.gif" /> 
             </div>
