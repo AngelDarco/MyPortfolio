a02afe06361e0cf45f70ef39667a4eb566908404
angeldarco <angel92-eme@hotmail.com>
2022-09-19 07:42:40 +0000

    darkmode updated

---
+++ src/pages/darkmode/DarkMode.jsx
@@ -0,0 +1,35 @@
+import './DarkMode.scss';
+import { MdDarkMode, MdOutlineWbSunny } from 'react-icons/md';
+import { useEffect, useState } from 'react';
+
+const DarkMode = ()=>{
+    const local = localStorage.getItem('darkmode') || false;
+    const [ darkMode, setDarkMode ] = useState(local);
+
+    const handlerDarkMode = ()=> {
+        setDarkMode(!darkMode);
+        darkMode
+        ? localStorage.setItem('darkmode','light')
+        : localStorage.setItem('darkmode','dark')
+    }
+    
+    if(local === 'dark'){
+        document.documentElement.style.setProperty('--primary-color', '#303131');
+        document.documentElement.style.setProperty('--secondary-color', '#000');
+        document.documentElement.style.setProperty('--text-color', '#fff');
+        document.documentElement.style.setProperty('--menu-color', '#000000b7');
+    }else{
+        document.documentElement.style.setProperty('--primary-color', '#e0dede');
+        document.documentElement.style.setProperty('--secondary-color', '#fff');
+        document.documentElement.style.setProperty('--text-color', '#000');
+        document.documentElement.style.setProperty('--menu-color', '#ffffffb7');
+    }
+
+    return(
+        <div className="containerDarkMode">
+            <MdDarkMode className={local === 'dark' && 'dark'} onClick={handlerDarkMode}/>
+            <MdOutlineWbSunny className={local === 'light' &&'light'} onClick={handlerDarkMode}/>
+        </div>
+    )
+}
+export default DarkMode;
