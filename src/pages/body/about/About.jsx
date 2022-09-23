533004a6fe984a861d9226d257a7a88758de6c5c
angeldarco <angel92-eme@hotmail.com>
2022-09-23 05:36:57 +0000

    about add

--- src/pages/body/about/About.jsx
+++ src/pages/body/about/About.jsx
@@ -1,26 +1,23 @@
 import './About.scss';
-import {ImHtmlFive} from 'react-icons/im';
-import {SiCss3,SiJavascript,SiPhp,SiPython,SiMysql} from 'react-icons/si';
-import {FaReact} from 'react-icons/fa';
 
 const About = ()=>{
     return(
-        <div className='containerAbout' >
-            <div className="body" id='about'>
-                <h1>About me</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, molestias aliquid mollitia fugit sint quos repellat sapiente illum atque consectetur eum in nostrum magnam, vero totam laborum quae dolore pariatur.</p>
-                <div className="icons">
-                    <ImHtmlFive/>
-                    <SiCss3/>
-                    <SiJavascript/>
-                    <FaReact/>
-                    <SiPhp/>
-                    <SiMysql/>
-                    <SiPython/>
-                </div>
-                <span> Angel Darco</span>
-            </div>
-        </div>
+        <div className="containerAbout">
+          <div className="body">
+              <h1>About me</h1>
+<div className="aboutText">
+<ul>
+<li> Hello, my name is Angel, I am 29 years old. </li>  
+          <li>I met the world of computing, at 12 years old, since then, I could never leave it. </li> 
+          <li>Like any young person who starts in this beautiful world, I took my first steps in the world of hacking.</li>
+          <li> The more I knew, the more problems I got into, so I started with design, editing and 3d modeling, but knowing how to use programs did not make me very happy, since I always wanted to be the one who creates the programs and not the one who pays to use them. </li>
+          <li> This led me to the world of programming, I took my first steps with Java, C# and Python.
+But Javascript was the language that captivated me, due to its extensive architecture, frameworks, the other languages with which it complements itself, the large number of projects you can create, and the large community it has, and with which you can learn, teach and compete.
+</li>
+</ul>
+</div>
+          </div>
+                  </div>
     )
 }
 export default About;
