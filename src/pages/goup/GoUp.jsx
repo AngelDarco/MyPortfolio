92124b34030f166fb40d0a452838db8c234703ea
angeldarco <angel92-eme@hotmail.com>
2022-09-27 11:55:16 +0000

    popup updated

--- src/pages/goup/GoUp.jsx
+++ src/pages/goup/GoUp.jsx
@@ -8,22 +8,32 @@ const GoUp = ({reference})=>{
         rootMargin: '0px',
         threshold: 1.0
     }
-    const observer = new IntersectionObserver((e)=>setShow(e[0].isIntersecting),options)
+    const observer = new IntersectionObserver((e)=>{
+        setShow(e[0].isIntersecting
+        )},options)
     reference && observer.observe(reference);
     return(
-        <div className="GoupContainer" style={styles}>
-            { !show && <BsArrowUpCircle onClick={()=>location.href = '#home'}/> }
+        <div className="GoupContainer" style={styles.container}>
+            <div style={styles.body} className="body">
+           { !show && <BsArrowUpCircle onClick={()=>location.href = '#home'}/> }
+            </div>
         </div>
     )
 }
 const styles = {
-    color: 'var(--text-color)',
-    borderRadius: '50%',
-    width: 'fit-content',
-    position: 'sticky',
-    top: '90%',
-    left: '90%',
-    fontSize: '3rem',
-    zIndex: '200'
+    container:{
+        width:'100%',
+        height:'10vh',
+        position: 'sticky',
+        top: '90%',
+        left: '90%',
+        zIndex: '200'
+    },
+    body:{
+        borderRadius: '50%',
+        color: 'var(--text-color)',
+        fontSize: '2.5rem',
+        padding:'0 5%'
+    }
 }
 export default GoUp;
