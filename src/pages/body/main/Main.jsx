be2168cc170beaa3551382d20e375604fb54d128
Angel Darco <angel92-eme@hotmail.com>
2023-03-13 02:32:43 -0500

    main section created

--- src/pages/body/main/Main.jsx
+++ src/pages/body/main/Main.jsx
@@ -1,20 +1,48 @@
+/* eslint-disable react/react-in-jsx-scope */
 import './Main.scss';
 import logo from '../../../assets/img/owner.png';
+import Header from '../header/Header';
+import Typewriter  from 'typewriter-effect/dist/core';
+import { useEffect, useRef } from 'react';
 
 const Main = ()=>{
+    const app = useRef(null);
+
+    useEffect(()=>{
+        if(app.current !== null){
+        const typewriter = new Typewriter(app.current,{
+            loop:true,
+            delay: 150,
+        });
+        typewriter
+        .pauseFor(2000)
+        .typeString('<span>Junior</span>')
+        .deleteChars(6)
+        .pauseFor(2000)
+        .typeString('<span>Mid/Level</span>')
+        .deleteChars(9)
+        .pauseFor(2000)
+        .typeString('<span>Senior</span>')
+        .start();
+        }
+    },[app.current])
+
     return (
         <div className='mainContainer' id='home'>
+            <Header/>
             <div className="body">
             <div className="logo">
                 <img src={logo} alt="logo.gif" /> 
             </div>
-            <div className="text">
+                <div className="text">
                 <h5></h5>
+                <p>Hi, I'm Angel and I'm:</p>
                 <h1>Frontend Developer</h1>
-                <h3>Mid/Level</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum temporibus eveniet aliquam culpa explicabo atque, incidunt tempora nihil exercitationem odit ratione in itaque, accusamus dolore voluptas, at facilis dolores voluptate.</p>
-                <h6></h6>
+            <div ref={app}>
             </div>
+                <h6></h6> 
+                </div>
+
             </div>
         </div>
     )
