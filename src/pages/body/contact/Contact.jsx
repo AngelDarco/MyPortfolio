caff0024bded6266dc6380049374fb443da5c292
Angel Darco <angel92-eme@hotmail.com>
2023-03-13 02:32:17 -0500

    contact section created

--- src/pages/body/contact/Contact.jsx
+++ src/pages/body/contact/Contact.jsx
@@ -1,8 +1,9 @@
 import './Contact.scss';
 import { MdAlternateEmail } from 'react-icons/md';
 import { AiFillGithub } from 'react-icons/ai';
+import { BsLinkedin } from 'react-icons/bs';
 import { SiGmail } from 'react-icons/si';
-import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
+import { FaFacebookF, FaTwitter, FaInstagram, FaFileDownload } from 'react-icons/fa';
 import { useState } from 'react';
 
 
@@ -21,25 +22,29 @@ const Contact = ()=>{
     }
 
     return(
-        <div className="containerContact" id='contact'>
+    <div className="containerContact" id='contact'>
             <div className="body">
                 {
                     showMessage && (
                     message ? <span className='message'>Copied</span>
-                            : <span className='message'>Sorry I don't use this red</span>
+                            : <span className='message'>Sorry I don't use this social network</span>
                         )
                             }
                 <h1>Contact</h1>
                 <div className="icons">
-                    <MdAlternateEmail onClick={()=>handlerIcons({e:true,content:'angel92-eme@hotmail.com'})}/>
-                    <AiFillGithub onClick={()=>handlerIcons({e:true,content:'@AngelDarco'})}/>
-                    <SiGmail onClick={()=>handlerIcons({e:true,content:'angelxcrema@gmail.com'})}/>
-                    <FaFacebookF onClick={()=>handlerIcons({e:false,content:''})}/>
-                    <FaTwitter onClick={()=>handlerIcons({e:false,content:''})}/>
-                    <FaInstagram onClick={()=>handlerIcons({e:false,content:''})}/>
+                    <MdAlternateEmail className='avalible' onClick={()=>handlerIcons({e:true,content:'angel92-eme@hotmail.com'})}/>
+                    <BsLinkedin className='avalible' onClick={()=>handlerIcons({e:true,content:'https://www.linkedin.com/in/angeldarco/'})}/>
+                    <AiFillGithub className='avalible'onClick={()=>handlerIcons({e:true,content:'https://github.com/AngelDarco/'})}/>
+                    <SiGmail className='avalible' onClick={()=>handlerIcons({e:true,content:'angelxcrema@gmail.com'})}/>
+                    <FaFacebookF className='notAvalible' onClick={()=>handlerIcons({e:false,content:''})}/>
+                    <FaTwitter className= 'notAvalible' onClick={()=>handlerIcons({e:false,content:''})}/>
+                    <FaInstagram className='notAvalible' onClick={()=>handlerIcons({e:false,content:''})}/>
+                    <abbr title="Download Resume">
+                    <FaFileDownload className='avalible' onClick={()=> window.open('https://firebasestorage.googleapis.com/v0/b/darcobbdd.appspot.com/o/Pdf%2FDarcoResume.pdf?alt=media&token=4b11663f-f0c5-4c8c-8c93-b191482ba63d','_blank')}/>
+                    </abbr>
                 </div>
             </div>
-        </div>
+    </div>
     )
 }
 export default Contact;
