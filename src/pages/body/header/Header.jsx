579f827d3b317f08cf5e1fce14938e1d4a735ce8
Angel Darco <angel92-eme@hotmail.com>
2023-06-02 10:12:44 -0500

    :rocket: main image and buttons styles optimized

--- src/pages/body/header/Header.jsx
+++ src/pages/body/header/Header.jsx
@@ -1,57 +1,57 @@
-import './Header.scss';
-import { useRef, useState } from 'react';
-import { GiHamburgerMenu } from 'react-icons/gi';
-import { GrClose } from 'react-icons/gr';
+import "./Header.scss";
+import { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { GrClose } from "react-icons/gr";
 
 const Header = ()=>{
-    const [ showMenu, setShowMenu ] = useState(false);
-    const handlerMenu = ()=> setShowMenu(!showMenu);
+	const [ showMenu, setShowMenu ] = useState(false);
+	const handlerMenu = ()=> setShowMenu(!showMenu);
     
-    const handlerLinks = (e)=>{
-        switch(e.target.innerText){
-            case 'observer':
-                window.location.href = '#observer';
-            break;
+	const handlerLinks = (e)=>{
+		switch(e.target.innerText){
+		case "observer":
+			window.location.href = "#observer";
+			break;
 
-            case 'PROYECTS':
-                window.location.href = '#proyects';
-            break;
+		case "PROJECTS":
+			window.location.href = "#projects";
+			break;
 
-            case 'ABOUT ME':
-                window.location.href = '#about';
-            break;
+		case "ABOUT ME":
+			window.location.href = "#about";
+			break;
 
-            case 'CONTACT':
-                window.location.href = '#contact';
-            break;
-            case 'HIRE ME':
-                window.location.href = '#contact';
-            break;
-            default: break;
-        }
-    }
+		case "CONTACT":
+			window.location.href = "#contact";
+			break;
+		case "HIRE ME":
+			window.location.href = "#contact";
+			break;
+		default: break;
+		}
+	};
 
-    return(
-        <div className="containerHeader">
-        <div className="body">
-        <div className='menu' onClick={handlerMenu}>
-                        {
-                           showMenu ? <GrClose className='close'/>
-                                    : <GiHamburgerMenu />
-                        }
-                    </div>
+	return(
+		<div className="containerHeader">
+			<div className="body">
+				<div className='menu' onClick={handlerMenu}>
+					{
+						showMenu ? <GrClose className='close'/>
+							: <GiHamburgerMenu />
+					}
+				</div>
 
-            <div className={`header ${!showMenu ? 'hide' : ''}`}>
-                <ul>
-                <li onClick={handlerLinks}>Home</li>
-                <li onClick={handlerLinks}>Proyects</li>
-                <li onClick={handlerLinks}>About me</li>
-                <li onClick={handlerLinks}>Contact</li>
-            </ul>
-            <button onClick={handlerLinks}>Hire Me</button>
-                </div>
-        </div>
-        </div>
-    )
-}
+				<div className={`header ${ showMenu ? "" : "hide" }`}>
+					<ul>
+						<li onClick={handlerLinks}>Home</li>
+						<li onClick={handlerLinks}>Projects</li>
+						<li onClick={handlerLinks}>About me</li>
+						<li onClick={handlerLinks}>Contact</li>
+					</ul>
+					<button onClick={handlerLinks}className="hover-spinner">Hire Me</button>
+				</div>
+			</div>
+		</div>
+	);
+};
 export default Header;
--- src/pages/body/main/Main.jsx
+++ src/pages/body/main/Main.jsx
@@ -1,50 +1,50 @@
-/* eslint-disable react/react-in-jsx-scope */
-import './Main.scss';
-import logo from '../../../assets/img/owner.png';
-import Header from '../header/Header';
-import Typewriter  from 'typewriter-effect/dist/core';
-import { useEffect, useRef } from 'react';
+/* eslint-disable react/no-unescaped-entities */
+import "./Main.scss";
+import logo from "../../../assets/img/owner.png";
+import Header from "../header/Header";
+import Typewriter  from "typewriter-effect/dist/core";
+import { useEffect, useRef } from "react";
 
 const Main = ()=>{
-    const app = useRef(null);
+	const app = useRef(null);
 
-    useEffect(()=>{
-        if(app.current !== null){
-        const typewriter = new Typewriter(app.current,{
-            loop:true,
-            delay: 150,
-        });
-        typewriter
-        .pauseFor(2000)
-        .typeString('<span>Junior</span>')
-        .deleteChars(6)
-        .pauseFor(2000)
-        .typeString('<span>Mid/Level</span>')
-        .deleteChars(9)
-        .pauseFor(2000)
-        .typeString('<span>Senior</span>')
-        .start();
-        }
-    },[app.current])
+	useEffect(()=>{
+		if(app.current !== null){
+			const typewriter = new Typewriter(app.current,{
+				loop:true,
+				delay: 150,
+			});
+			typewriter
+				.pauseFor(2000)
+				.typeString("<span>Junior</span>")
+				.deleteChars(6)
+				.pauseFor(2000)
+				.typeString("<span>Mid/Level</span>")
+				.deleteChars(9)
+				.pauseFor(2000)
+				.typeString("<span>Senior</span>")
+				.start();
+		}
+	},[app.current]);
 
-    return (
-        <div className='mainContainer' id='home'>
-            <Header/>
-            <div className="body">
-            <div className="logo">
-                <img src={logo} alt="logo.gif" /> 
-            </div>
-                <div className="text">
-                <h5></h5>
-                <p>Hi, I'm Angel and I'm:</p>
-                <h1>Frontend Developer</h1>
-            <div ref={app}>
-            </div>
-                <h6></h6> 
-                </div>
+	return (
+		<div className='mainContainer' id='home'>
+			<Header/>
+			<div className="body">
+				<div className="logo">
+					<img src={logo} alt="logo.gif" /> 
+				</div>
+				<div className="text">
+					<h5></h5>
+					<p>Hi, I'm Angel and I'm:</p>
+					<h1>Frontend Developer</h1>
+					<div ref={app}>
+					</div>
+					<h6></h6> 
+				</div>
 
-            </div>
-        </div>
-    )
-}
+			</div>
+		</div>
+	);
+};
 export default Main;
