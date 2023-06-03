f2cc128af3d82a8f8a5dfc620981a6c80eb621b6
Angel Darco <angel92-eme@hotmail.com>
2023-06-03 13:22:30 -0500

    :rocket: projects styles and new database added

---
+++ src/database/database.js
@@ -0,0 +1,59 @@
+import img02 from "../assets/img/web02.png";
+import img03 from "../assets/img/web03.jpg";
+import img04 from "../assets/img/web04.jpg";
+import img05 from "../assets/img/web05.jpg";
+import img06 from "../assets/img/web06.jpg";
+import img07 from "../assets/img/web07.jpg";
+
+const database = [
+	{
+		"id": 1,
+		"name": "Clothes Store",
+		"img": img02,
+		"link": "https://github.com/AngelDarco/Web_Page-04",
+		"desc": "Responsive Clothes Store created with ThreeJs models",
+		"lenguajes": ["▹ html","▹ css","▹ js", "▹ React three fiber"]
+	},
+	{
+		"id": 2,
+		"name": "Library",
+		"img": img03,
+		"link": "https://github.com/AngelDarco/FirebaseCrud",
+		"desc": "CRUD in firebase",
+		"lenguajes": ["▹ html","▹ css","▹ js", "▹ React","▹ sass","▹ firebase"]
+	},
+	{
+		"id": 3,
+		"name": "Test Recruters",
+		"img": img04,
+		"link": "https://github.com/AngelDarco/jobInterviewTest",
+		"desc": "Web page for a job interview in indeed",
+		"lenguajes": ["▹ html","▹ css","▹ js"]
+	},
+	{
+		"id": 4,
+		"name": "Netflix Clone",
+		"img": img05,
+		"link": "https://github.com/AngelDarco/Css_Netflix_Copia",
+		"desc": "Clone of Netflix page",
+		"lenguajes": ["▹ html","▹ css","▹ js"]
+	},
+	{
+		"id": 5,
+		"name": "Boutique",
+		"img": img06,
+		"link": "https://github.com/AngelDarco/TiendaRopa",
+		"desc": "e-commerce with database of users, like and shoping car",
+		"lenguajes": ["▹ html","▹ css","▹ js","▹ php","▹ mysql","▹ sql","▹ webpack"]
+	},
+	{
+		"id": 6,
+		"name": "E-Commerce",
+		"img": img07,
+		"link": "https://github.com/AngelDarco/Proyecto_Branda",
+		"desc": "E-commerce with database of registration of users, login and shopping car in mysql",
+		"lenguajes": ["▹ html","▹ css","▹ js","▹ node modules","▹ php","▹ mysql","▹ sql","▹ eslint","▹ prettier","▹ webpack"]
+	}
+];
+
+export default database;
--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -5,22 +5,22 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper";
 import { BiCodeAlt } from "react-icons/bi";
 import { MdOutlineKeyboardDoubleArrowRight } from "react-icons/md";
+import database from "../../database/database";
 
 function Projects() {
 	const [data, setData] = useState();
 
-	useEffect(() => {
-		fetch("./src/database/database.json")
-			.then((res) => res.json())
-			.then((res) => setData(res))
-			.catch((err) => console.log(err));
-	}, []);
+	useEffect(() => { if (database) setData(database);}, []);
 
-	const handlerState = (id)=>{
-		if(!data) return;
-		const i = data.findIndex( obj => obj.id === id);
+	// function to add states to each item in the database
+	const handlerState = (id) => {
+		if (!data) return;
+		const i = data.findIndex((obj) => obj.id === id);
 		const arr = [...data];
-		arr[i] = { ...arr[i], isClicked: arr[i].isClicked ? !arr[i].isClicked : true};
+		arr[i] = {
+			...arr[i],
+			isClicked: arr[i].isClicked ? !arr[i].isClicked : true,
+		};
 		setData(arr);
 	};
 
@@ -30,49 +30,47 @@ function Projects() {
 				<h1>Some of my Projects</h1>
 				<Swiper
 					className="cards mySwiper"
-					autoplay={
-						{
-							delay: 10000,
-							disableOnInteraction: true,
-						}
-					}
-					modules={
-						[Autoplay]
-					}
+					autoplay={{
+						delay: 10000,
+						disableOnInteraction: true,
+					}}
+					modules={[Autoplay]}
 				>
-					{
-						data?.map((res) => (
-							<SwiperSlide className="mainCard" key={res.id}>
-								<div className="card">
-									<img src={res.img} alt={res.name} onClick={()=>handlerState(res.id)} />
-									{
-										!res.isClicked && <div className="description">
-											<h1>{res.name}</h1>
-											<div className="about">
-												<span>{res.description}</span>
-											</div>
-											<h5>Tecnolgies</h5>
-											<ul>
-												{
-													res?.lenguajes?.map((itm) => <li key={res.id * Math.random() * 100}>{itm}</li>)
-												}
-											</ul>
-										</div> 
-									}
-									<div className="navigation">
-										<div onClick={() => window.open(res.code)}>
-											<BiCodeAlt className="code" />
-											<span>code</span>
+					{data?.map((res) => (
+						<SwiperSlide className="mainCard" key={res.id}>
+							<div className="card">
+								<img
+									src={res.img}
+									alt={res.name}
+									onClick={() => handlerState(res.id)}
+								/>
+								{!res.isClicked && (
+									<div className="description">
+										<h1>{res.name}</h1>
+										<div className="about">
+											<span>{res.description}</span>
 										</div>
-										<div onClick={() => window.open(res.browse)}>
-											<MdOutlineKeyboardDoubleArrowRight className="browse" />
-											<span>online</span>
-										</div>
+										<h5>Tecnolgies</h5>
+										<ul>
+											{res?.lenguajes?.map((itm) => (
+												<li key={res.id * Math.random() * 100}>{itm}</li>
+											))}
+										</ul>
+									</div>
+								)}
+								<div className="navigation">
+									<div onClick={() => window.open(res.code)}>
+										<BiCodeAlt className="code" />
+										<span>code</span>
+									</div>
+									<div onClick={() => window.open(res.browse)}>
+										<MdOutlineKeyboardDoubleArrowRight className="browse" />
+										<span>online</span>
 									</div>
 								</div>
-							</SwiperSlide>
-						))
-					}
+							</div>
+						</SwiperSlide>
+					))}
 				</Swiper>
 			</div>
 		</div>
