cad13b9605b20fec0ac53c7aee589053264d0a14
Angel Darco <angel92-eme@hotmail.com>
2023-06-18 14:42:06 -0500

    :rocket: new eslint config, and svg's styles added

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,41 +1,25 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"es2021": true
-	},
-	"extends": [
-		"eslint:recommended",
-		"plugin:react/recommended"
-	],
-	"overrides": [
-	],
-	"parserOptions": {
-		"ecmaVersion": "latest",
-		"sourceType": "module"
-	},
-	"plugins": [
-		"react"
-	],
-	"rules": {
-		"react/react-in-jsx-scope": "off",
-		"indent": [
-			"error",
-			"tab"
-		],
-		"linebreak-style": [
-			"error",
-			"unix"
-		],
-		"quotes": [
-			"error",
-			"double"
-		],
-		"semi": [
-			"error",
-			"always"
-		]
-	},
-	"globals":{
-		// "someModule": true, //disable module is not defined
-	}
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [ "eslint:recommended", "plugin:react/recommended" ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: [ "react" ],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    indent: [ "warn", 2 ],
+    "object-curly-spacing": [ "warn", "always" ],
+    "array-bracket-spacing": [ "warn", "always" ],
+    "linebreak-style": [ "error", "unix" ],
+    quotes: [ "warn", "double" ],
+    semi: [ "error", "always" ],
+  },
+  globals: {
+    // "someModule": true, //disable module is not defined
+  },
 };
