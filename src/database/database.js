48fb3b54052af33b052b8506f9ac5e519934aaaf
Angel Darco <angel92-eme@hotmail.com>
2023-06-18 14:40:00 -0500

    :sparkles: database tables updated and new images added

--- src/database/database.js
+++ src/database/database.js
@@ -4,56 +4,72 @@ import img04 from "../assets/img/web04.jpg";
 import img05 from "../assets/img/web05.jpg";
 import img06 from "../assets/img/web06.jpg";
 import img07 from "../assets/img/web07.jpg";
+import img08 from "../assets/img/web08.jpg";
 
 const database = [
-	{
-		"id": 1,
-		"name": "Clothes Store",
-		"img": img02,
-		"link": "https://github.com/AngelDarco/Web_Page-04",
-		"description": "Responsive Clothes Store created with ThreeJs models",
-		"lenguajes": ["▹ html","▹ css","▹ js", "▹ React three fiber"]
-	},
-	{
-		"id": 2,
-		"name": "Library",
-		"img": img03,
-		"link": "https://github.com/AngelDarco/FirebaseCrud",
-		"description": "CRUD in firebase",
-		"lenguajes": ["▹ html","▹ css","▹ js", "▹ React","▹ sass","▹ firebase"]
-	},
-	{
-		"id": 3,
-		"name": "Test Recruters",
-		"img": img04,
-		"link": "https://github.com/AngelDarco/jobInterviewTest",
-		"description": "Web page for a job interview in indeed",
-		"lenguajes": ["▹ html","▹ css","▹ js"]
-	},
-	{
-		"id": 4,
-		"name": "Netflix Clone",
-		"img": img05,
-		"link": "https://github.com/AngelDarco/Css_Netflix_Copia",
-		"description": "Clone of Netflix page",
-		"lenguajes": ["▹ html","▹ css","▹ js"]
-	},
-	{
-		"id": 5,
-		"name": "Boutique",
-		"img": img06,
-		"link": "https://github.com/AngelDarco/TiendaRopa",
-		"description": "e-commerce with database of users, like and shoping car",
-		"lenguajes": ["▹ html","▹ css","▹ js","▹ php","▹ mysql","▹ sql","▹ webpack"]
-	},
-	{
-		"id": 6,
-		"name": "E-Commerce",
-		"img": img07,
-		"link": "https://github.com/AngelDarco/Proyecto_Branda",
-		"description": "E-commerce with database of registration of users, login and shopping car in mysql",
-		"lenguajes": ["▹ html","▹ css","▹ js","▹ node modules","▹ php","▹ mysql","▹ sql","▹ eslint","▹ prettier","▹ webpack"]
-	}
+  {
+    "id": 7,
+    "name": "Chat app",
+    "img": img08,
+    "code": "https://github.com/AngelDarco/chat-firebase",
+    "browse": "https://cozy-semifreddo-c8ba95.netlify.app/",
+    "description": "Real time chat app",
+    "lenguajes": [ "▹ React","▹ post-css","▹ Typescript", "▹ vite","▹ vitest","▹ eslint","▹ firebase" ]
+  },
+  {
+    "id": 1,
+    "name": "Clothes Store",
+    "img": img02,
+    "code": "https://github.com/AngelDarco/Web_Page-04",
+    "browse": " http://darcocorporation.scienceontheweb.net/pages/webs/page03_clothesStore/",
+    "description": "Responsive Clothes Store created with ThreeJs models",
+    "lenguajes": [ "▹ html","▹ css","▹ js", "▹ React three fiber" ]
+  },
+  {
+    "id": 2,
+    "name": "Library",
+    "img": img03,
+    "code": "https://github.com/AngelDarco/FirebaseCrud",
+    "browse": "http://darcocorporation.scienceontheweb.net/pages/webs/page04_Library/",
+    "description": "CRUD in firebase",
+    "lenguajes": [ "▹ html","▹ css","▹ js", "▹ React","▹ sass","▹ firebase" ]
+  },
+  {
+    "id": 3,
+    "name": "Test Recruters",
+    "img": img04,
+    "code": "https://github.com/AngelDarco/jobInterviewTest",
+    "browse": "http://darcocorporation.scienceontheweb.net/pages/webs/page05_TestRecruters/",
+    "description": "Web page for a job interview in indeed",
+    "lenguajes": [ "▹ html","▹ css","▹ js" ]
+  },
+  {
+    "id": 4,
+    "name": "Netflix Clone",
+    "img": img05,
+    "code": "https://github.com/AngelDarco/Css_Netflix_Copia",
+    "browse": "http://darcocorporation.scienceontheweb.net/pages/webs/page06_Netflix/",
+    "description": "Clone of Netflix page",
+    "lenguajes": [ "▹ html","▹ css","▹ js" ]
+  },
+  {
+    "id": 5,
+    "name": "Boutique",
+    "img": img06,
+    "code": "https://github.com/AngelDarco/TiendaRopa",
+    "browse": "http://darcocorporation.scienceontheweb.net/pages/webs/page07_Boutique/",
+    "description": "e-commerce with database of users, like and shoping car",
+    "lenguajes": [ "▹ html","▹ css","▹ js","▹ php","▹ mysql","▹ sql","▹ webpack" ]
+  },
+  {
+    "id": 6,
+    "name": "E-Commerce",
+    "img": img07,
+    "code": "https://github.com/AngelDarco/Proyecto_Branda",
+    "browse": "http://darcocorporation.scienceontheweb.net/pages/webs/page08_BrandaStore/",
+    "description": "E-commerce with database of registration of users, login and shopping car in mysql",
+    "lenguajes": [ "▹ html","▹ css","▹ js","▹ node modules","▹ php","▹ mysql","▹ sql","▹ eslint","▹ prettier","▹ webpack" ]
+  }
 ];
 
 export default database;
