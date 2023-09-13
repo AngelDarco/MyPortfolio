f1975a185049d722193e513883d73013a0eb69e9
Angel Darco <angel92-eme@hotmail.com>
2023-09-13 10:32:14 -0500

    :bug: incorrect routes fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,31 @@
-import './App.css';
-import Proyects from './pages/projects/Projects';
-import About from './pages/body/about/About';
-import Footer from "./pages/body/footer/Footer"
-import Main from "./pages/body/main/Main"
-import Contact from './pages/body/contact/Contact';
-import DarkMode from './pages/darkmode/DarkMode';
-import Skills from './pages/body/skills/Skills'
-import { useEffect, useRef, useState } from 'react';
-import GoUp from './pages/goup/GoUp';
+import "./App.css";
+import Proyects from "./pages/projects/Projects";
+import About from "./pages/body/about/About";
+import Footer from "./pages/body/footer/Footer";
+import Main from "./pages/body/main/Main";
+import Contact from "./pages/body/contact/Contact";
+import DarkMode from "./pages/darkmode/DarkMode";
+import Skills from "./pages/body/skills/Skills";
+import { useEffect, useRef, useState } from "react";
+import GoUp from "./pages/goup/GoUp";
 
 function App() {
   const app = useRef();
   const [ node, setNode ] = useState(null);
-  useEffect(()=> setNode(app.current),[])
+  useEffect(()=> setNode(app.current),[]);
 
   return (
     <div  className="App">
       <DarkMode/>
-       <Main/>
-       <div ref={app} style={{position:'absolute', top:'15%'}} ></div>
+      <Main/>
+      <div ref={app} style={{ position:"absolute", top:"15%" }} ></div>
       { <GoUp reference={node} /> }
-       <About/>
-       <Skills/>
-       <Proyects/>
-       <Contact/>
+      <About/>
+      <Skills/>
+      <Proyects/>
+      <Contact/>
       <Footer/>
     </div>
-  )
+  );
 }
 export default App;
--- src/pages/body/main/Main.jsx
+++ src/pages/body/main/Main.jsx
@@ -6,45 +6,45 @@ import Typewriter  from "typewriter-effect/dist/core";
 import { useEffect, useRef } from "react";
 
 const Main = ()=>{
-	const app = useRef(null);
+  const app = useRef(null);
 
-	useEffect(()=>{
-		if(app.current !== null){
-			const typewriter = new Typewriter(app.current,{
-				loop:true,
-				delay: 150,
-			});
-			typewriter
-				.pauseFor(2000)
-				.typeString("<span>Junior</span>")
-				.deleteChars(6)
-				.pauseFor(2000)
-				.typeString("<span>Mid/Level</span>")
-				.deleteChars(9)
-				.pauseFor(2000)
-				.typeString("<span>Senior</span>")
-				.start();
-		}
-	},[app.current]);
+  useEffect(()=>{
+    if(app.current !== null){
+      const typewriter = new Typewriter(app.current,{
+        loop:true,
+        delay: 150,
+      });
+      typewriter
+        .pauseFor(2000)
+        .typeString("<span>Junior</span>")
+        .deleteChars(6)
+        .pauseFor(2000)
+        .typeString("<span>Mid/Level</span>")
+        .deleteChars(9)
+        .pauseFor(2000)
+        .typeString("<span>Senior</span>")
+        .start();
+    }
+  },[ app.current ]);
 
-	return (
-		<div className='mainContainer' id='home'>
-			<Header/>
-			<div className="body">
-				<div className="logo">
-					<img src={logo} alt="logo.gif" /> 
-				</div>
-				<div className="text">
-					<h5></h5>
-					<p>Hi, I'm Angel and I'm:</p>
-					<h1>Frontend Developer</h1>
-					<div ref={app}>
-					</div>
-					<h6></h6> 
-				</div>
+  return (
+    <div className='mainContainer' id='home'>
+      <Header/>
+      <div className="body">
+        <div className="logo">
+          <img src={logo} alt="logo.gif" /> 
+        </div>
+        <div className="text">
+          <h5></h5>
+          <p>Hi there, I'm Angel and I'm:</p>
+          <h1>Frontend Developer</h1>
+          <div ref={app}>
+          </div>
+          <h6></h6> 
+        </div>
 
-			</div>
-		</div>
-	);
+      </div>
+    </div>
+  );
 };
 export default Main;
