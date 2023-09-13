fe9bc66da361442068ec9cff51b1c2878134f23e
Angel Darco <angel92-eme@hotmail.com>
2023-09-13 10:33:28 -0500

    :sparkles: new project youtube-twin added

--- src/database/database.js
+++ src/database/database.js
@@ -1,41 +1,51 @@
-import img02 from "../assets/img/web02.png";
-import img03 from "../assets/img/web03.jpg";
-import img04 from "../assets/img/web04.jpg";
-import img05 from "../assets/img/web05.jpg";
-import img06 from "../assets/img/web06.jpg";
-import img07 from "../assets/img/web07.jpg";
-import img08 from "../assets/img/web08.jpg";
+import img01 from "../assets/img/web02.png";
+import img02 from "../assets/img/web03.jpg";
+import img03 from "../assets/img/web04.jpg";
+import img04 from "../assets/img/web05.jpg";
+import img05 from "../assets/img/web06.jpg";
+import img06 from "../assets/img/web07.jpg";
+import img07 from "../assets/img/web08.jpg";
+import img08 from "../assets/img/web09.jpg";
 
 const database = [
   {
+    "id": 8,
+    "name": "Youtube Clone",
+    "img": img08,
+    "code": "https://github.com/AngelDarco/Css_Youtube_Twin",
+    "browse": "https://youtube-twin.up.railway.app/",
+    "description": "A Clone of the Youtube page, with css only, no frameworks",
+    "lenguajes": [ "▹ html","▹ css","▹ js" ]
+  },
+  {
     "id": 7,
     "name": "Chat app",
-    "img": img08,
+    "img": img07,
     "code": "https://github.com/AngelDarco/chat-firebase",
     "browse": "https://cozy-semifreddo-c8ba95.netlify.app/",
     "description": "Real time chat app",
     "lenguajes": [ "▹ React","▹ post-css","▹ Typescript", "▹ vite","▹ vitest","▹ eslint","▹ firebase" ]
   },
   {
-    "id": 1,
+    "id": 6,
     "name": "Clothes Store",
-    "img": img02,
+    "img": img06,
     "code": "https://github.com/AngelDarco/Web_Page-04",
     "browse": " http://darcocorporation.scienceontheweb.net/pages/webs/page03_clothesStore/",
     "description": "Responsive Clothes Store created with ThreeJs models",
     "lenguajes": [ "▹ html","▹ css","▹ js", "▹ React three fiber" ]
   },
   {
-    "id": 2,
+    "id": 5,
     "name": "Library",
-    "img": img03,
+    "img": img05,
     "code": "https://github.com/AngelDarco/FirebaseCrud",
     "browse": "http://darcocorporation.scienceontheweb.net/pages/webs/page04_Library/",
     "description": "CRUD in firebase",
     "lenguajes": [ "▹ html","▹ css","▹ js", "▹ React","▹ sass","▹ firebase" ]
   },
   {
-    "id": 3,
+    "id": 4,
     "name": "Test Recruters",
     "img": img04,
     "code": "https://github.com/AngelDarco/jobInterviewTest",
@@ -44,32 +54,32 @@ const database = [
     "lenguajes": [ "▹ html","▹ css","▹ js" ]
   },
   {
-    "id": 4,
+    "id": 3,
     "name": "Netflix Clone",
-    "img": img05,
+    "img": img03,
     "code": "https://github.com/AngelDarco/Css_Netflix_Copia",
     "browse": "http://darcocorporation.scienceontheweb.net/pages/webs/page06_Netflix/",
     "description": "Clone of Netflix page",
     "lenguajes": [ "▹ html","▹ css","▹ js" ]
   },
   {
-    "id": 5,
+    "id": 2,
     "name": "Boutique",
-    "img": img06,
+    "img": img02,
     "code": "https://github.com/AngelDarco/TiendaRopa",
     "browse": "http://darcocorporation.scienceontheweb.net/pages/webs/page07_Boutique/",
     "description": "e-commerce with database of users, like and shoping car",
     "lenguajes": [ "▹ html","▹ css","▹ js","▹ php","▹ mysql","▹ sql","▹ webpack" ]
   },
   {
-    "id": 6,
+    "id": 1,
     "name": "E-Commerce",
-    "img": img07,
+    "img": img01,
     "code": "https://github.com/AngelDarco/Proyecto_Branda",
     "browse": "http://darcocorporation.scienceontheweb.net/pages/webs/page08_BrandaStore/",
     "description": "E-commerce with database of registration of users, login and shopping car in mysql",
     "lenguajes": [ "▹ html","▹ css","▹ js","▹ node modules","▹ php","▹ mysql","▹ sql","▹ eslint","▹ prettier","▹ webpack" ]
-  }
+  },
 ];
 
 export default database;
