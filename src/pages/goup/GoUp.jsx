4ab6b87b1c81384a27292713630f517b313ca7f6
AngelDarco <angel92-eme@hotmail.com>
2024-01-26 12:42:27 -0500

    :racehorse: component to scroll to top refactored and optimized

--- src/pages/goup/GoUp.jsx
+++ src/pages/goup/GoUp.jsx
@@ -1,39 +1,47 @@
-import { useState } from 'react';
-import { BsArrowUpCircle } from 'react-icons/bs';
+import { useState } from "react";
+import { BsArrowUpCircle } from "react-icons/bs";
 
-const GoUp = ({reference})=>{
-   const [ show, setShow ] = useState(true);
-   const options = {
-        root: null,
-        rootMargin: '0px',
-        threshold: 1.0
-    }
-    const observer = new IntersectionObserver((e)=>{
-        setShow(e[0].isIntersecting
-        )},options)
-    reference && observer.observe(reference);
-    return(
-        <div className="GoupContainer" style={styles.container}>
-            <div style={styles.body} className="body">
-           { !show && <BsArrowUpCircle onClick={()=>location.href = '#home'}/> }
-            </div>
-        </div>
-    )
-}
+const GoUp = ({ reference, container }) => {
+  const [show, setShow] = useState(true);
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 1.0,
+  };
+  const observer = new IntersectionObserver((e) => {
+    setShow(e[0].isIntersecting);
+  }, options);
+  reference && observer.observe(reference);
+
+  const scrollToTop = () => {
+    if (container) container.scrollIntoView({ behavior: "smooth" });
+  };
+
+  return (
+    <div style={styles.container}>
+      <div style={styles.body}>
+        {!show && (
+          <BsArrowUpCircle onClick={scrollToTop} style={styles.arrow} />
+        )}
+      </div>
+    </div>
+  );
+};
+
 const styles = {
-    container:{
-        width:'100%',
-        height:'10vh',
-        position: 'sticky',
-        top: '90%',
-        left: '90%',
-        zIndex: '200'
-    },
-    body:{
-        borderRadius: '50%',
-        color: 'var(--text-color)',
-        fontSize: '2.5rem',
-        padding:'0 5%'
-    }
-}
+  container: {
+    width: "100%",
+    height: "10vh",
+    position: "fixed",
+    top: "90%",
+    zIndex: "200",
+  },
+  body: {
+    borderRadius: "50%",
+    color: "var(--text-color)",
+    fontSize: "2.5rem",
+    padding: "0 5%",
+  },
+};
+
 export default GoUp;
