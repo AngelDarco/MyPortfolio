38e050c0f83aaff9bbe5ef59d54cd310ff4b85d1
AngelDarco <angel92-eme@hotmail.com>
2024-01-26 12:43:33 -0500

    :racehorse: component to scroll to top refactored and optimized

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,33 @@ import About from "./pages/body/about/About";
 import Footer from "./pages/body/footer/Footer";
 import Main from "./pages/body/main/Main";
 import Contact from "./pages/body/contact/Contact";
-import DarkMode from "./pages/darkmode/DarkMode";
 import Skills from "./pages/body/skills/Skills";
 import { useEffect, useRef, useState } from "react";
 import GoUp from "./pages/goup/GoUp";
+// import Cursors from "darco-cursors";
+import DarkMode from "./pages/darkmode/DarkMode";
 
 function App() {
   const app = useRef();
-  const [ node, setNode ] = useState(null);
-  useEffect(()=> setNode(app.current),[]);
+  const containerRef = useRef();
+
+  const [node, setNode] = useState(null);
+
+  useEffect(() => setNode(app.current), []);
 
   return (
-    <div  className="App">
-      <DarkMode/>
-      <Main/>
-      <div ref={app} style={{ position:"absolute", top:"15%" }} ></div>
-      { <GoUp reference={node} /> }
-      <About/>
-      <Skills/>
-      <Proyects/>
-      <Contact/>
-      <Footer/>
+    // <Cursors className="App">
+    <div ref={containerRef} className="App">
+      <DarkMode />
+      <Main />
+      <div ref={app} style={{ position: "absolute", top: "15%" }}></div>
+      {<GoUp reference={node} container={containerRef.current} />}
+      <About />
+      <Skills />
+      <Proyects />
+      <Contact />
+      <Footer />
+      {/* </Cursors> */}
     </div>
   );
 }
