122bde6010fc62017e873babdc14255161a234d0
AngelDarco <angel92-eme@hotmail.com>
2024-01-28 23:56:36 -0500

    :wreck: eslint config updated

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,25 @@
+// eslint-disable-next-line no-undef
 module.exports = {
   env: {
     browser: true,
     es2021: true,
   },
-  extends: [ "eslint:recommended", "plugin:react/recommended" ],
+  extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
   overrides: [],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  plugins: [ "react" ],
+  plugins: ["react"],
   rules: {
     "react/react-in-jsx-scope": "off",
-    indent: [ "warn", 2 ],
-    "object-curly-spacing": [ "warn", "always" ],
-    "array-bracket-spacing": [ "warn", "always" ],
-    "linebreak-style": [ "error", "unix" ],
-    quotes: [ "warn", "double" ],
-    semi: [ "error", "always" ],
+    semi: ["error", "always"],
+    "array-bracket-spacing": ["error", "always"],
   },
-  globals: {
-    // "someModule": true, //disable module is not defined
+  overrides: {
+    rules: {
+      "array-bracket-spacing": ["error", "always"],
+      // ... other rules}
+    },
   },
 };
