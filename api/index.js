ea3398954ca8280ec6d143ef39fa6f2cd2231c7d
AngelDarco <angel92-eme@hotmail.com>
2024-02-17 06:53:47 -0500

    :sparkles: vercel serverless function to send contact messages added

---
+++ api/index.js
@@ -0,0 +1,75 @@
+/* eslint-disable no-undef */
+import { Resend } from "resend";
+
+export async function POST(request) {
+  const NEXT_PUBLIC_RESEND_API_KEY = "re_9gxxTLem_BumgzGoQjEL36LBJCVWHGNiv";
+
+  const resend = new Resend(NEXT_PUBLIC_RESEND_API_KEY);
+
+  try {
+    const { name, subject, email, message } = await request.json();
+
+    const { data, error } = await resend.emails.send({
+      from: "Portfolio <onboarding@resend.dev>",
+      to: ["angelxcrema@gmail.com"],
+      subject: subject,
+      html: `
+      <html>
+      <style>
+        body{
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          flex-direction: column;
+          width: 100%;
+          height: 100dvh;
+          flex-direction: column;
+          gap: 20px;
+          outline: 1px solid #0F0;
+          background-color: #333;
+          color: white;
+          position: relative;
+        }
+        h3{
+          color:orangered
+        }
+        span{
+          color: purple; 
+          position: absolute; 
+          bottom: 20px;
+          left: 20px
+        }
+      </styles>
+      <body>
+      <h3>
+            <i>Subject: </i>${subject}
+          </h3>
+          <p>
+            <strong>
+              <i>Message from: </i>
+              ${name}
+            </strong><br/>
+            ${message}
+          </p>
+          <span>
+            <strong>
+              <i>From:</i>
+            </strong>
+            ${email}
+          </span>
+        </body>
+        </html>  
+      `,
+    });
+
+    console.log(data);
+
+    if (error) return new Response(error, { headers });
+    return new Response(JSON.stringify(data), { headers });
+  } catch (error) {
+    return new Response("Error processing the request", {
+      status: 500,
+      headers,
+    });
+  }
+}
