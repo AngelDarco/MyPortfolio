9a0b3cd4cdea85c7b4c988f96c56ca71b2f90c12
AngelDarco <angel92-eme@hotmail.com>
2024-02-21 08:46:58 -0500

    :racehorse: darkmode refactored and added new variables

--- src/pages/darkmode/DarkMode.jsx
+++ src/pages/darkmode/DarkMode.jsx
@@ -1,36 +1,33 @@
 import Darkmode from "darco-dark-mode";
 import "./DarkMode.scss";
 
-const DarkMode = () => {
+const DarkMode = ({ isDark }) => {
   const switchStylesVariables = [
     {
       theme: "Light",
-      variables: [
-        "--primary-color",
-        "--secondary-color",
-        "--text-color",
-        "--color",
-      ],
-      values: ["#e0dede", "#fff", "#000", "#ffffffb7"],
+      variables: ["--bg-color", "--text-color", "--bg-menu-color"],
+      values: ["rgba(255, 255, 255, 0.3)", "#000", "#ffffffd1"]
     },
     {
       theme: "Dark",
-      variables: [
-        "--primary-color",
-        "--secondary-color",
-        "--text-color",
-        "--menu-color",
-      ],
-      values: ["#303131", "#000", "#fff", "#000000b7"],
-    },
+      variables: ["--bg-color", "--text-color", "--bg-menu-color"],
+      values: ["rgba(0, 0, 0, 0.1)", "#fff", "#000000eb"]
+    }
   ];
 
   return (
     <header>
       <Darkmode
+        isDark={isDark}
         switchStyles={switchStylesVariables}
         icons
-        styles={{ mainContainer: { borderColor: "var(--text-color)" } }}
+        styles={{
+          mainContainer: {
+            borderColor: "var(--text-color)",
+            height: "30px",
+            width: "30px"
+          }
+        }}
       />
     </header>
   );
