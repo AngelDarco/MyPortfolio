102574c84687cd7ea87099e098af2978eebc1735
AngelDarco <angel92-eme@hotmail.com>
2024-02-22 11:54:31 -0500

    :sparkles: darkmode variables moduled

--- src/pages/darkmode/DarkMode.jsx
+++ src/pages/darkmode/DarkMode.jsx
@@ -1,25 +1,13 @@
 import Darkmode from "darco-dark-mode";
 import "./DarkMode.scss";
+import darkmodeVariables from "../../utils/darkmodeVariables.js";
 
 const DarkMode = ({ isDark }) => {
-  const switchStylesVariables = [
-    {
-      theme: "Light",
-      variables: ["--bg-color", "--text-color", "--bg-menu-color"],
-      values: ["rgba(255, 255, 255, 0.3)", "#000", "#ffffffd1"]
-    },
-    {
-      theme: "Dark",
-      variables: ["--bg-color", "--text-color", "--bg-menu-color"],
-      values: ["rgba(0, 0, 0, 0.1)", "#fff", "#000000eb"]
-    }
-  ];
-
   return (
     <header>
       <Darkmode
         isDark={isDark}
-        switchStyles={switchStylesVariables}
+        switchStyles={darkmodeVariables}
         icons
         styles={{
           mainContainer: {
---
+++ src/utils/darkmodeVariables.js
@@ -0,0 +1,24 @@
+const darkmodeVariables = [
+  {
+    theme: "Light",
+    variables: [
+      "--bg-color",
+      "--text-color",
+      "--bg-menu-color",
+      "--upBtn-color"
+    ],
+    values: ["rgba(255, 255, 255, 0.3)", "#000", "#ffffffd1", "#b07ee1"]
+  },
+  {
+    theme: "Dark",
+    variables: [
+      "--bg-color",
+      "--text-color",
+      "--bg-menu-color",
+      "--upBtn-color"
+    ],
+    values: ["rgba(0, 0, 0, 0.4)", "#fff", "#000000eb", "#7a19db"]
+  }
+];
+
+export default darkmodeVariables;
