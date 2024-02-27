d631788b42d53673fcf25323e77ca9a356bcc0bd
AngelDarco <angel92-eme@hotmail.com>
2024-02-27 10:01:42 -0500

    :lipstick: css variables added

--- src/utils/darkmodeVariables.js
+++ src/utils/darkmodeVariables.js
@@ -2,24 +2,26 @@ const darkmodeVariables = [
   {
     theme: "Light",
     variables: [
+      "--main-bg-color",
       "--bg-color",
       "--text-color",
       "--bg-menu-color",
       "--box-shadow",
       "--upBtn-color"
     ],
-    values: ["#fff", "#000", "#ffffffd1", "#00000029", "#b07ee1"]
+    values: ["#fff", "#fff0", "#000", "#ffffffd1", "#00000029", "#b07ee1"]
   },
   {
     theme: "Dark",
     variables: [
+      "--main-bg-color",
       "--bg-color",
       "--text-color",
       "--bg-menu-color",
       "--box-shadow",
       "--upBtn-color"
     ],
-    values: ["#000", "#fff", "#000000eb", "#ffffff29", "#7a19db"]
+    values: ["#000", "#0001", "#fff", "#000000eb", "#ffffff29", "#7a19db"]
   }
 ];
 
