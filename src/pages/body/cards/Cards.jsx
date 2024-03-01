40dbae04a6970bffa76424a75e0dfc39c856723a
AngelDarco <angel92-eme@hotmail.com>
2024-02-29 23:32:52 -0500

    :racehorse: cards component moduled on card component

--- src/pages/body/cards/Cards.jsx
+++ src/pages/body/cards/Cards.jsx
@@ -1,12 +1,11 @@
 import { useEffect, useRef, useState } from "react";
 import "./Cards.scss";
 import intersectionObserver from "../../../utils/intersectionObserver";
-import Typewriter from "typewriter-effect/dist/core";
+import Card from "./Card";
 
 export default function Cards() {
   const [cards, setCards] = useState();
   const refCard = useRef();
-  const refCard1 = useRef();
 
   useEffect(() => {
     if (!refCard.current) return;
@@ -17,50 +16,36 @@ export default function Cards() {
     };
   }, []);
 
-  useEffect(() => {
-    if (cards && refCard1.current) {
-      const typewriter = new Typewriter(refCard1.current, {
-        loop: false,
-        delay: 50,
-        cursor: ""
-      });
-      typewriter.pauseFor(1000).typeString("<h4>Mid/Level</h4>").start();
-    }
-  }, [cards]);
-
   return (
     <section ref={refCard} className={`cardsContainer`}>
-      <div
+      <Card
         style={{
           transform: cards ? "translateX(0)" : "translateX(-200%)",
           transition: "transform 3s"
         }}
-        className="card"
-      >
-        <h1>Cards</h1>
-        <p ref={refCard1}></p>
-      </div>
-      <div
+        title="Test Card 01"
+      />
+      <Card
         style={{
           transform: cards ? "translateY(0)" : "translateY(-210%)",
           transition: "transform 3s"
         }}
-        className="card"
-      ></div>
-      <div
+        title="card02"
+      />
+      <Card
         style={{
           transform: cards ? "translateY(0)" : "translateY(210%)",
           transition: "transform 3s"
         }}
-        className="card"
-      ></div>
-      <div
+        title="card4"
+      />
+      <Card
         style={{
           transform: cards ? "translateX(0)" : "translateX(200%)",
           transition: "transform 3s"
         }}
-        className="card"
-      ></div>
+        title="card5"
+      />
     </section>
   );
 }
